e409d2c57bd5734d88657834259798c3cbb25d86
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-12-18 16:43:56 -0500

    changes jsx and react icons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-import ChallengeCard from './components/ChallengeCard/ChallengeCard';
+import App from './App';
+//import ChallengeCard from './components/ChallengeCard/ChallengeCard';
 
 
-ReactDOM.render(<ChallengeCard />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
