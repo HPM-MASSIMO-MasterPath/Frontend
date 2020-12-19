f1ffaeaa368c6e601268c49ad404dd79ba9f1ec0
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-12-18 20:02:25 -0600

    Adding travis

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Layout from './layout/Layout.jsx'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 
