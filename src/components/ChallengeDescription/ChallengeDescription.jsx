d11b4678660d5b5e9c4ec70e47c2dd8a7af537fb
Deymer Hernández <deymerh@hotmail.com>
2020-12-19 06:11:59 -0500

    Prepared modal window

--- src/components/ChallengeDescription/ChallengeDescription.jsx
+++ src/components/ChallengeDescription/ChallengeDescription.jsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { API } from '../../utils/config.jsx';
+import ModalComponent from '../ModalComponent/ModalComponent.jsx';
 
 import './ChallengeDescription.scss';
 
 const ChallengeDescription = () => {
   const [description, setDescription] = useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false);
   let { id } = useParams();
   // const id = 6;
+
+  const openModal = () => {
+    setIsOpen(!modalIsOpen);
+  }
 
   useEffect(() => {
     const getDescription = () => {
@@ -23,28 +29,34 @@ const ChallengeDescription = () => {
   });
 
   return (
-    <div className='description'>
-      <div className='description__title'>
-        <h2>
-          |CHALLENGE <span>|EASY</span>
-        </h2>
-      </div>
-      <div className='description__card'>
-        <div className='description__card-text'>
-          <h3>{description.name}</h3>
-          <h4>{description.difficulty}</h4>
-          <p>
-            {description.description} <br /> OUTPUT: <br /> {description.output}
-          </p>
+    <>
+      <div className='description'>
+        <div className='description__title'>
+          <h2>
+            |CHALLENGE <span>|EASY</span>
+          </h2>
         </div>
-        <div className='description__card-button'>
-          <button>SOLVE CHALLENGE</button>
+        <div className='description__card'>
+          <div className='description__card-text'>
+            <h3>{description.name}</h3>
+            <h4>{description.difficulty}</h4>
+            <p>
+              {description.description} <br /> OUTPUT: <br /> {description.output}
+            </p>
+          </div>
+          <div className='description__card-button'>
+            <button onClick={openModal}>SOLVE CHALLENGE</button>
+          </div>
+        </div>
+        <div className='description__send'>
+          <input type='text' placeholder='Your Github repo Link' />
         </div>
       </div>
-      <div className='description__send'>
-        <input type='text' placeholder='Your Github repo Link' />
-      </div>
-    </div>
+      { modalIsOpen && <ModalComponent
+        modalIsOpen={modalIsOpen}
+        onClick={openModal}
+      />}
+    </>
   );
 };
 
