0ba38b9dfd0bf4f1cf996770f3260dee6694b047
Luis Chavez <josephsiul15@gmail.com>
2020-12-19 15:50:56 -0600

    Login finished

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import Axios from 'axios';
+// import Axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import './Login.scss';
 import log_img from '../../assets/images/log_im.png';
@@ -11,25 +12,35 @@ const Login = () => {
   const [login, setLogin] = useState({
     username: '',
     password: '',
-  })
-  const onLogin = (e) => {
-    // e.preventDefault();
-    fetch(`${API}api/login`, JSON.stringify({
-      username: login.username,
-      password: login.password
-    }))
-      .then(res => {
-        console.log(res)
-      })
-  }
+  });
+  const [err, setError] = useState({ error: false });
+  let history = useHistory();
+
+  const onLogin = e => {
+    e.preventDefault();
+
+    console.log('login', login);
+    fetch(`${API}api/login`, {
+      method: 'POST',
+      body: JSON.stringify(login),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(response => response.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => {
+        if (response === 'unauthorized') {
+          setError({ error: true });
+        } else {
+          history.push('/home');
+        }
+      });
+  };
   const handleInputs = ({ target }) => {
     setLogin({
       ...login,
-      [target.name]: target.value
-    })
-    console.log(login.username)
-    console.log(login.password)
-  }
+      [target.name]: target.value,
+    });
+  };
   return (
     <div className='login'>
       <div className='login__container'>
@@ -37,21 +48,34 @@ const Login = () => {
           <img src={log_img} alt='' />
         </div>
         <div className='login__form'>
-          <form form={`${API}api/login`} onSubmit={onLogin}>
+          <form>
             <div className='login__form--header'>
               <img src={lozi} alt='' />
               <h1>Sign In</h1>
             </div>
             <div className='login__form--group'>
               <label htmlFor='inputEmail'>Email Adress</label>
-              <input name="username" type='text' placeholder='Enter your email' onChange={handleInputs} />
+              <input
+                name='username'
+                type='text'
+                placeholder='Enter your email'
+                onChange={handleInputs}
+              />
             </div>
             <div className='login__form--group'>
               <label htmlFor='inputPassword'>Password</label>
-              <input name="password" type='password' placeholder='Enter your password' onChange={handleInputs} />
+              <input
+                name='password'
+                type='password'
+                placeholder='Enter your password'
+                onChange={handleInputs}
+              />
             </div>
             <div className='login__form--group'>
-              <button type='submit'>Sign in</button>
+              <button type='button' onClick={onLogin}>
+                Sign in
+              </button>
+              {err.error && <p>Bad Credentials</p>}
             </div>
           </form>
         </div>
