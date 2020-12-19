b1b4a830b77911b65d6028df4e9a1432f42b8fee
Deymer Hernández <deymerh@hotmail.com>
2020-12-19 02:18:40 -0500

    Link to Home

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import ChallengeCard from './components/ChallengeCard/ChallengeCard';
-// import ChallengeLayout from './components/ChallengeLayout/ChallengeLayout';
 
 ReactDOM.render(<App />, document.getElementById('app'));
