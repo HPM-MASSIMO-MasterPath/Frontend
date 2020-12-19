3f205b8cdb9fc8ee8b49a3897eee74cb25801252
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-12-19 07:37:29 -0500

    login laayout and styles

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 
 import './Login.scss';
+import log_img from '../../assets/images/log_im.png';
+import lozi from '../../assets/images/logo-platzimaster.png';
 
 const Login = () => {
   return (
-    <h1>Login</h1>
+      <div className="login">
+        <div className="login__container">
+        <div className="login__image">
+          <img src={log_img} alt=""/>
+        </div>
+        <div className="login__form">
+          <form action="">
+            <div className="login__form--header">
+              <img src={lozi} alt=""/>
+              <h1>Sign In</h1>
+            </div>
+            <div className="login__form--group">
+              <label htmlFor="inputEmail">Email Adress</label>
+              <input type="text" placeholder="Enter your email" />
+            </div>
+            <div className="login__form--group">
+              <label htmlFor="inputPassword">Password</label>
+              <input type="text" placeholder="Enter your password" />
+            </div>
+            <div className="login__form--group">
+            <button type="submit">Sign in</button>
+            </div>
+          </form>
+        </div>
+        </div>
+      </div>
   );
 };
 
